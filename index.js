cd1eef643bec3554a719a7f76c5782894b2134a5
Abumercy <101097127+Abumercy@users.noreply.github.com>
2023-03-26 22:28:55 +0100

    changes made

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-console.log("Hello, I'm ChatGPT.");
-console.log("My preferred programming language is JavaScript.");
-console.log(
-  "In the next year, I see myself continuing to learn and improve my skills as a language model, and providing helpful responses to users like you!"
-);
-
-const name = "ChatGPT";
+const name = "Mercy Abu";
 const preferredLanguage = "JavaScript";
 const futureGoal =
-  "In the next 1 year, I see myself continuing to assist and provide helpful responses to users as an AI language model.";
+  "In the next 1 year, I see myself continuing to learn and improve my skills";
 
 console.log(
   `My name is ${name}. My preferred programming language is ${preferredLanguage}. ${futureGoal}`
